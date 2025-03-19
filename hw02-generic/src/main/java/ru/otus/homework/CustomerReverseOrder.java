c3d8620c66bf23bb20532e0a10daca53f7d8332b
Yanin Vyacheslav <slava.vy.2006@gmail.com>
2025-03-19 13:52:36 +0300

    hw2 refactoring

--- hw02-generic/src/main/java/ru/otus/homework/CustomerReverseOrder.java
+++ hw02-generic/src/main/java/ru/otus/homework/CustomerReverseOrder.java
@@ -1,9 +1,10 @@
 package ru.otus.homework;
 
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class CustomerReverseOrder {
-    private final LinkedList<Customer> customers;
+    private final Deque<Customer> customers;
 
     public CustomerReverseOrder() {
         customers = new LinkedList<>();
