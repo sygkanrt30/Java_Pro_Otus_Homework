777a4957cdb7bce036747861ee6d00b38d7e8db9
Yanin Vyacheslav <slava.vy.2006@gmail.com>
2025-03-30 20:22:08 +0300

    hw3 fixed

--- hw03-annotation/src/main/java/ru/otus/homework/Main.java
+++ hw03-annotation/src/main/java/ru/otus/homework/Main.java
@@ -1,9 +1,13 @@
 package ru.otus.homework;
 
 import ru.otus.homework.my_junit.AnnotationProcessor;
+import ru.otus.homework.my_junit.TestResult;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        AnnotationProcessor.run(MyJunitTest.class);
+        TestResult testResult = AnnotationProcessor.run(MyJunitTest.class);
+        testResult.printResultOfTestClass();
+        TestResult testResult2 = AnnotationProcessor.run(MySecondJunitTest.class);
+        testResult2.printResultOfTestClass();
     }
 }
--- hw03-annotation/src/main/java/ru/otus/homework/my_junit/AnnotationProcessor.java
+++ hw03-annotation/src/main/java/ru/otus/homework/my_junit/AnnotationProcessor.java
@@ -5,46 +5,46 @@ import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import ru.otus.homework.MyJunitTest;
-
 public class AnnotationProcessor {
-    private static final Logger log = LoggerFactory.getLogger(AnnotationProcessor.class);
     private static final Set<Method> AFTER_METHODS = new HashSet<>();
     private static final Set<Method> BEFORE_METHODS = new HashSet<>();
     private static final Set<Method> TEST_METHODS = new HashSet<>();
-    private static int countNotPassedTestMethod = 0;
+    private static int COUNT_NOT_PASSED_TEST_METHODS = 0;
 
-    public static void run(Class<MyJunitTest> clazz) throws Exception {
+    public static TestResult run(Class<?> clazz) throws Exception {
         groupMethodsByAnnotations(clazz);
         executeLifeCycle(clazz);
-        log.info(
-                "Total test methods: {}; Passed test methods: {}; Failed test methods: {}",
-                TEST_METHODS.size(),
-                TEST_METHODS.size() - countNotPassedTestMethod,
-                countNotPassedTestMethod);
+        TestResult testResult = new TestResult(TEST_METHODS.size(), COUNT_NOT_PASSED_TEST_METHODS);
+        resetField();
+        return testResult;
     }
 
-    private static void executeLifeCycle(Class<MyJunitTest> clazz) throws Exception {
+    private static void executeLifeCycle(Class<?> clazz) throws Exception {
         for (Method method : TEST_METHODS) {
-            Constructor<MyJunitTest> constructor = clazz.getConstructor();
-            MyJunitTest testClass = constructor.newInstance();
-            executeBeforeAndAfter(BEFORE_METHODS, testClass);
-            executeTestMethod(method, testClass);
-            executeBeforeAndAfter(AFTER_METHODS, testClass);
+            Constructor<?> constructor = clazz.getConstructor();
+            Object testClass = constructor.newInstance();
+            executeBeforeAndAfterEach(BEFORE_METHODS, testClass);
+            executeTestMethod(method, testClass, true);
+            executeBeforeAndAfterEach(AFTER_METHODS, testClass);
         }
     }
 
-    private static void executeTestMethod(Method method, MyJunitTest testClass) {
+    private static void resetField() {
+        AFTER_METHODS.clear();
+        BEFORE_METHODS.clear();
+        TEST_METHODS.clear();
+        COUNT_NOT_PASSED_TEST_METHODS = 0;
+    }
+
+    private static void executeTestMethod(Method method, Object testClass, boolean isTest) {
         try {
             method.invoke(testClass);
         } catch (Exception e) {
-            countNotPassedTestMethod++;
+            if (isTest) COUNT_NOT_PASSED_TEST_METHODS++;
         }
     }
 
-    private static void groupMethodsByAnnotations(Class<MyJunitTest> clazz) {
+    private static void groupMethodsByAnnotations(Class<?> clazz) {
         Method[] methods = clazz.getMethods();
         for (Method method : methods) {
             if (method.isAnnotationPresent(Before.class)) {
@@ -57,9 +57,9 @@ public class AnnotationProcessor {
         }
     }
 
-    private static void executeBeforeAndAfter(Set<Method> methods, MyJunitTest clazz) throws Exception {
+    private static void executeBeforeAndAfterEach(Set<Method> methods, Object clazz) {
         for (Method method : methods) {
-            method.invoke(clazz);
+            executeTestMethod(method, clazz, false);
         }
     }
 }
